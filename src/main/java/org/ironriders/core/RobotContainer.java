f0c7d1bc32a31b677767cde15e86255839b623c5
a-mishmash <reldamail@icloud.com>
2025-01-06 18:10:47 -0800

    Added drive vendordeps

--- src/main/java/org/ironriders/core/RobotContainer.java
+++ src/main/java/org/ironriders/core/RobotContainer.java
@@ -2,13 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.ironriders.core;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import org.ironriders.drive.DriveCommands;
+import org.ironriders.drive.DriveConstants;
+import org.ironriders.drive.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -20,11 +20,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+  private final DriveCommands driveCommands = driveSubsystem.getCommands();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController primaryController =
+      new CommandXboxController(DriveConstants.PRIMARY_CONTROLLER_PORT);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,13 +42,22 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    driveSubsystem.setDefaultCommand(
+        driveCommands.driveTeleop(
+            () -> Utils.controlCurve(
+              primaryController.getLeftX(), 
+              DriveConstants.TRANSLATION_CONTROL_EXPONENT, 
+              DriveConstants.TRANSLATION_CONTROL_DEADBAND),
+            () -> Utils.controlCurve(
+              primaryController.getLeftY(), 
+              DriveConstants.TRANSLATION_CONTROL_EXPONENT, 
+              DriveConstants.TRANSLATION_CONTROL_DEADBAND),
+            () -> Utils.controlCurve(
+              primaryController.getLeftX(), 
+              DriveConstants.ROTATION_CONTROL_EXPONENT, 
+              DriveConstants.ROTATION_CONTROL_DEADBAND)
+        )
+    );
   }
 
   /**
@@ -57,7 +66,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    // An example command will be run in autonomous. THIS IS A PLACEHOLDER!
+    return Commands.none();
   }
 }
---
+++ src/main/java/org/ironriders/core/Utils.java
@@ -0,0 +1,16 @@
+package org.ironriders.core;
+
+public class Utils {
+
+    /**
+     * Applies a control curve (currently just an exponential function)
+     * Only works with input values from 0 to 1 because 1^x = 1.
+     * 
+     * @param input The value to put into the curve (0.0 - 1.0 ONLY)
+     * @param deadband The exponent value.
+     * @return The end result of the curve.
+     */
+    public static double controlCurve(double input, double exponent, double deadband) {
+                return Math.pow(input, exponent);
+    }
+}
