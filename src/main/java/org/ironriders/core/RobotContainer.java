915a91034ba08ddbb50a46b524a7c7177f2b34c3
a-mishmash <reldamail@icloud.com>
2025-01-13 16:38:57 -0800

    Added RobotCommands class

--- src/main/java/org/ironriders/core/RobotContainer.java
+++ src/main/java/org/ironriders/core/RobotContainer.java
@@ -10,10 +10,8 @@ import org.ironriders.drive.DriveSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.*;
 import com.pathplanner.lib.auto.AutoBuilder;
 
 /**
