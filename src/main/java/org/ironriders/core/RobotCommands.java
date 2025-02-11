c57404575d4009da477dfb1054bc7146f1db2387
a-mishmash <reldamail@icloud.com>
2025-02-10 17:48:06 -0800

    merge fixes

--- src/main/java/org/ironriders/core/RobotCommands.java
+++ src/main/java/org/ironriders/core/RobotCommands.java
@@ -16,11 +16,11 @@ import org.ironriders.drive.DriveSubsystem;
 import org.ironriders.drive.DriveConstants;
 import org.ironriders.vision.VisionCommands;
 import org.ironriders.vision.VisionSubsystem;
-
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import java.util.function.Supplier;
 
 /**
  * These commands require more complex logic and are not directly tied to a
@@ -64,9 +64,9 @@ public class RobotCommands {
 
         return driveCommands.drive(
                 () -> new Translation2d(
-                        inputTranslationX.getAsDouble() * DriveConstants.SWERVE_MAXIMUM_SPEED,
-                        inputTranslationY.getAsDouble() * DriveConstants.SWERVE_MAXIMUM_SPEED),
-                () -> inputRotation.getAsDouble() * DriveConstants.SWERVE_MAXIMUM_SPEED,
+                        inputTranslationX.getAsDouble() * DriveConstants.SWERVE_MAXIMUM_SPEED_TELEOP,
+                        inputTranslationY.getAsDouble() * DriveConstants.SWERVE_MAXIMUM_SPEED_TELEOP),
+                () -> inputRotation.getAsDouble() * DriveConstants.SWERVE_MAXIMUM_SPEED_TELEOP,
                 () -> true
         );
     }
