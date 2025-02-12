95147de92a1981044228ae7460ad39e2c136c381
a-mishmash <reldamail@icloud.com>
2025-02-12 15:02:46 -0800

    minor restructure to elevator

--- src/main/java/org/ironriders/core/RobotCommands.java
+++ src/main/java/org/ironriders/core/RobotCommands.java
@@ -14,6 +14,7 @@ import org.ironriders.coral.CoralWristCommands;
 import org.ironriders.coral.CoralWristSubsystem;
 import org.ironriders.drive.DriveCommands;
 import org.ironriders.drive.DriveSubsystem;
+import org.ironriders.elevator.ElevatorCommands;
 import org.ironriders.drive.DriveConstants;
 import org.ironriders.vision.VisionCommands;
 import org.ironriders.vision.VisionSubsystem;
@@ -35,6 +36,7 @@ import java.util.function.Supplier;
 public class RobotCommands {
 
     private final DriveCommands driveCommands;
+    private final ElevatorCommands elevatorCommands;
     private final CoralWristCommands coralWristCommands;
     private final CoralIntakeCommands coralIntakeCommands;
     private final AlgaeWristCommands algaeWristCommands;
@@ -46,6 +48,7 @@ public class RobotCommands {
             CoralIntakeCommands coralIntakeCommands, AlgaeWristCommands algaeWristCommands,
             AlgaeIntakeCommands algaeIntakeCommands, VisionCommands visionCommands, GenericHID controller) {
         this.driveCommands = driveCommands;
+        this.elevatorCommands = elevatorCommands;
         this.coralWristCommands = coralWristCommands;
         this.coralIntakeCommands = coralIntakeCommands;
         this.algaeWristCommands = algaeWristCommands;
--- src/main/java/org/ironriders/elevator/ElevatorConstants.java
+++ src/main/java/org/ironriders/elevator/ElevatorConstants.java
@@ -1,31 +1,47 @@
 package org.ironriders.elevator;
 
 public class ElevatorConstants {
-    public static final int leftElevatorMotorDeviceID = 0;
-    public static final int rightElevatorMotorDeviceID = 0;
 
-    public static final double gearRatio = 0;
-    public static final double sprocketDiameter = 0;
+    public static final int LEFT_MOTOR_ID = 0;
+    public static final int RIGHT_MOTOR_ID = 0;
 
-    public static final double L1Height = 0;
-    public static final double L2Height = 0;
-    public static final double L3Height = 0;
-    public static final double L4Height = 0;
-    
-    public static final double p = 0;
-    public static final double i = 0;
-    public static final double d = 0;
-    public static final double iZone = 0;
+    public static final double GEAR_RATIO = 0;
+    public static final double SPROCKET_DIAMETER = 0;
 
-    public static final double maxVel = 0;
-    public static final double maxAcc = 0;
+    public static final double HEIGHT_L1 = 0; // inches
+    public static final double HEIGHT_L2 = 0;
+    public static final double HEIGHT_L3 = 0;
+    public static final double HEIGHT_L4 = 0;
 
-    public static final double t = .2;
-    public static final double kS = 0;
-    public static final double kG = 0;
-    public static final double kV = 0;
-    public static final double kA = 0;
+    public static final double MOTOR_PID_P = 0;
+    public static final double MOTOR_PID_I = 0;
+    public static final double MOTOR_PID_D = 0;
+    public static final double MOTOR_PID_IZONE = 0;
 
-    public static final double ffVel = 0;
-    public static final double ffAcc = 0;
+    public static final double GOAL_TOLERANCE = 0.1; // same unit as heights
+
+    public static final double MAXIMUM_VELOCITY = 0;
+    public static final double MAXIMUM_ACCELERATION = 0;
+
+    public static final double T = .2;
+    public static final double KS = 0;
+    public static final double KG = 0;
+    public static final double KV = 0;
+    public static final double KA = 0;
+
+    public static final double FF_VEL = 0;
+    public static final double FF_ACC = 0;
+
+    public enum Level {
+        L1(HEIGHT_L1),
+        L2(HEIGHT_L2),
+        L3(HEIGHT_L3),
+        L4(HEIGHT_L4);
+
+        public double height;
+
+        private Level(double height) {
+            this.height = height;
+        }
+    }
 }
