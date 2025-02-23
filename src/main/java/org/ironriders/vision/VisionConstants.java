98eaf10f2f4a03eddee9dc7cb42a53b7219749ca
a-mishmash <reldamail@icloud.com>
2025-02-23 13:16:33 -0800

    changes

--- src/main/java/org/ironriders/vision/VisionConstants.java
+++ src/main/java/org/ironriders/vision/VisionConstants.java
@@ -9,11 +9,8 @@ import edu.wpi.first.math.util.Units;
 public class VisionConstants {
 
     public static final Camera[] CAMERAS = {
-            new Camera("front", new Transform3d(new Translation3d(Units.inchesToMeters(14), 0.0, 6.5), new Rotation3d()))
+            new Camera("front", new Transform3d(new Translation3d(Units.inchesToMeters(14), 0.0, 0.0), new Rotation3d()))
     };
-
-    public static final Pose2d[] STATION_LOCATIONS_RED = { new Pose2d() };
-    public static final Pose2d[] STATION_LOCATIONS_BLUE = { new Pose2d() };
 
     /** represents a name and offset for convenience/simplicity */
     public static class Camera {
