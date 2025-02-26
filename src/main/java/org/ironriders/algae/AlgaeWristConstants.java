2407d73ac0dc2db4cc744df4fad3362987305719
isabelle <lshields822@gmail.com>
2025-02-25 18:51:41 -0800

    small

--- src/main/java/org/ironriders/algae/AlgaeWristConstants.java
+++ src/main/java/org/ironriders/algae/AlgaeWristConstants.java
@@ -17,7 +17,7 @@ public class AlgaeWristConstants {
     public static final double GEAR_RATIO = 0.01;
     public static final double MIN_POSITION =0;
     public static final double MAX_POSITION =100;
-    public static final double MAX_ACC = 10;
+    public static final double MAX_ACC = 30;
     public static final double MAX_VEL = 30;
 
 
