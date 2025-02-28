e91283dd80571e1eebaa09b1f6121e05b9862140
iprobablydontknowwhatimdoing <octact82@gmail.com>
2025-02-27 21:14:23 -0800

    comment

--- src/main/java/org/ironriders/climb/ClimbConstants.java
+++ src/main/java/org/ironriders/climb/ClimbConstants.java
@@ -3,8 +3,8 @@ package org.ironriders.climb;
 public class ClimbConstants {
     public static double liftTime = 3;
     public static boolean stopPressed = false;
-    public static final int CLIMBER_MOTOR = 17;
-
+    public static final int CLIMBER_MOTOR = 17; // probably what the ID will be, but we'll see when it's assigned TODO
+    
     public enum State {
         UP(0.1),
         DOWN(-0.1);
