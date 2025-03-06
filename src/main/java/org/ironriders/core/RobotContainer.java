d3f7d91a2c4196e9fad25c03194470d2a063e244
a-mishmash <reldamail@icloud.com>
2025-03-06 13:22:34 -0800

    Amend 2: Electric Boogaloo

--- src/main/java/org/ironriders/core/RobotContainer.java
+++ src/main/java/org/ironriders/core/RobotContainer.java
@@ -221,6 +221,8 @@ public class RobotContainer {
 			});
 		}));
 
+		primaryController.y().onTrue(driveCommands.pathfindToTarget());
+
 		// Configure dpad as jog control. wpilib exposes dpad as goofy "pov" values
 		// which are an angle; we create a trigger for each discrete 45-degree angle
 		for (var angle = 0; angle < 360; angle += 45) {
