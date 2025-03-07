b21f464ad63b1c02fb30d906a66a1fede004be5d
saturncorgi <tylerclayton183@gmail.com>
2025-03-06 16:13:00 -0800

    add all homed smartdashboard bool

--- src/main/java/org/ironriders/core/Robot.java
+++ src/main/java/org/ironriders/core/Robot.java
@@ -5,6 +5,7 @@
 package org.ironriders.core;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -28,6 +29,7 @@ public class Robot extends TimedRobot {
 	@Override
 	public void robotPeriodic() {
 		CommandScheduler.getInstance().run();
+		SmartDashboard.putBoolean("All Homed", SmartDashboard.getBoolean("Subsystems/Elevator/Homed",false)&&SmartDashboard.getBoolean("Subsystems/AlgaeWrist/Homed",false)&&SmartDashboard.getBoolean("Subsystems/CoralWrist/Homed",false));
 	}
 
 	@Override
