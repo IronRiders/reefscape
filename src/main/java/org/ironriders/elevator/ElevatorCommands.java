71409b4c6ca535ac261b2ae5b4cfde9866dc6e9c
a-mishmash <reldamail@icloud.com>
2025-03-10 13:15:09 -0700

    aa

--- src/main/java/org/ironriders/elevator/ElevatorCommands.java
+++ src/main/java/org/ironriders/elevator/ElevatorCommands.java
@@ -1,9 +1,6 @@
 package org.ironriders.elevator;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
-import edu.wpi.first.wpilibj2.command.Commands;
-
 import org.ironriders.elevator.ElevatorConstants.*;
 
 public class ElevatorCommands {
@@ -13,33 +10,39 @@ public class ElevatorCommands {
     public ElevatorCommands(ElevatorSubsystem elevator) {
         this.elevatorSubsystem = elevator;
 
-        elevator.publish("Home", home());
-        elevator.publish("Elevator rising to L1", set(Level.L1));
-        elevator.publish("Elevator rising to L2", set(Level.L2));
-        elevator.publish("Elevator rising to L3", set(Level.L3));
-        elevator.publish("Elevator rising to L4", set(Level.L4));
-        elevator.publish("Elevator going home", set(Level.Down));
-        elevator.publish("Elevator going to Coral Station", set(Level.CoralStation));
+        elevator.publish("Rehome", home());
+
+        elevator.publish("Elevator to L1", set(Level.L1));
+        elevator.publish("Elevator to L2", set(Level.L2));
+        elevator.publish("Elevator to L3", set(Level.L3));
+        elevator.publish("Elevator to L4", set(Level.L4));
+
+        elevator.publish("Elevator to Coral Station", set(Level.CoralStation));
+        elevator.publish("Elevator Down", set(Level.Down));
     }
 
     public Command set(ElevatorConstants.Level level) {
-        return elevatorSubsystem.runOnce(() -> {
-            elevatorSubsystem.setPositionInches(level.positionInches);
-        })
-                .andThen(Commands.waitUntil(() -> { return elevatorSubsystem.isAtPosition(level); }))
-                .handleInterrupt(elevatorSubsystem::reset);
+        return new Command() {
+            public void execute() {
+                elevatorSubsystem.setGoal(level);
+            }
+
+            public boolean isFinished() {
+                return elevatorSubsystem.isAtPosition(level);
+            }
+        };
     }
 
     public Command home() {
-        // If elevator is already homed, move to home position
-        if (elevatorSubsystem.isHomed()) {
-            return set(Level.Down);
-        }
+        return elevatorSubsystem.defer(() -> {
 
-        elevatorSubsystem.reportInfo("Homing");
+            if (elevatorSubsystem.isHomed()) {
+                return set(Level.Down);
+            }
+    
+            elevatorSubsystem.reportInfo("Homing");
 
-        return elevatorSubsystem.defer(
-            () -> new Command() {
+            return new Command() {
                 public void execute() {
                     elevatorSubsystem.setMotor(-0.1);
                 }
@@ -52,19 +55,11 @@ public class ElevatorCommands {
                     elevatorSubsystem.stopMotor();
                     elevatorSubsystem.reportHomed();
                 }
-            }
-        );
-    }
-
-    public Command jogCommand(boolean jogUp){
-        return elevatorSubsystem.runOnce(() -> {
-            elevatorSubsystem.jog(jogUp);;
+            };
         });
     }
 
-    public Command reset(){
+    public Command reset() {
         return elevatorSubsystem.runOnce(elevatorSubsystem::reset);
     }
 }
-
-    
