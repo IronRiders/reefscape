71153c1cc34597317e6fef9f6d19830715d61a40
isabelle <lshields822@gmail.com>
2025-03-10 10:31:30 -0700

    small 2

--- src/main/java/org/ironriders/climb/ClimbConstants.java
+++ src/main/java/org/ironriders/climb/ClimbConstants.java
@@ -1,13 +1,10 @@
 package org.ironriders.climb;
 
 public class ClimbConstants {
-
-    public static final double LIFT_TIME = 3;
     public static final int CLIMBER_MOTOR_CAN_ID = 17;
     public static final int COMPENSATION = 12;
     public static final int CURRENT_LIMIT = 40;
 
-    public static final int FAKE_PID_COMPENSATION = 1; //TODO: TUNE
     public static final double MAX_POSITION = 50;
     public static final double GEAR_RATIO = 1/100;
 
