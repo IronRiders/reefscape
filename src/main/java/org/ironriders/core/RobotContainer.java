f4b6f1f8ba39e67fe2675b977371f1f2e299e0b7
t <tylerclayton183@gmail.com>
2025-03-13 16:40:57 -0700

    80% done with new controls

--- src/main/java/org/ironriders/core/RobotContainer.java
+++ src/main/java/org/ironriders/core/RobotContainer.java
@@ -4,6 +4,8 @@
 
 package org.ironriders.core;
 
+import static org.ironriders.wrist.coral.CoralWristConstants.FORWARD_LIMIT;
+
 import org.ironriders.climb.ClimbCommands;
 import org.ironriders.climb.ClimbConstants;
 import org.ironriders.climb.ClimbSubsystem;
@@ -25,6 +27,7 @@ import org.ironriders.wrist.algae.AlgaeIntakeSubsystem;
 import org.ironriders.wrist.algae.AlgaeWristCommands;
 import org.ironriders.wrist.algae.AlgaeWristConstants;
 import org.ironriders.wrist.algae.AlgaeWristSubsystem;
+import org.ironriders.wrist.algae.AlgaeWristConstants.AlgaeWristState;
 import org.ironriders.wrist.coral.CoralIntakeCommands;
 import org.ironriders.wrist.coral.CoralIntakeConstants;
 import org.ironriders.wrist.coral.CoralIntakeSubsystem;
@@ -39,6 +42,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -80,7 +84,7 @@ public class RobotContainer {
 
 	private final CommandXboxController primaryController = new CommandXboxController(
 			DriveConstants.PRIMARY_CONTROLLER_PORT);
-	private final CommandGenericHID secondaryController = new CommandGenericHID(DriveConstants.KEYPAD_CONTROLLER_PORT);
+	private final CommandGenericHID secondaryController = new CommandJoystick(DriveConstants.KEYPAD_CONTROLLER_PORT);
 
 	public final RobotCommands robotCommands = new RobotCommands(
 			driveCommands, targetingCommands, elevatorCommands,
@@ -120,13 +124,29 @@ public class RobotContainer {
 
 		// slows down drivetrain when pressed
 		primaryController.leftTrigger().onTrue(driveCommands.setDriveTrainSpeed(true)).onFalse(driveCommands.setDriveTrainSpeed(false));
+		// jog commands on pov buttons
+		for (var angle = 0; angle < 360; angle += 45) {
+			primaryController.pov(angle).onTrue(driveCommands.jog(-angle));
+		}
+		// y vision align station not implmented yet //TODO
+		// x vision align reef not implmented yet //TODO
 
-		primaryController.leftBumper().onTrue(driveCommands.jog(0));
+		//Secondary Driver left side buttons
+		secondaryController.button(5).onTrue(robotCommands.moveElevatorAndWrist(ElevatorConstants.Level.L1));
+		secondaryController.button(6).onTrue(robotCommands.moveElevatorAndWrist(ElevatorConstants.Level.L2));
+		secondaryController.button(7).onTrue(robotCommands.moveElevatorAndWrist(ElevatorConstants.Level.L3));
+		secondaryController.button(8).onTrue(robotCommands.moveElevatorAndWrist(ElevatorConstants.Level.L4));
+		secondaryController.button(9).onTrue(robotCommands.moveElevatorAndWrist(ElevatorConstants.Level.CoralStation));
+		secondaryController.button(10).onTrue(robotCommands.moveElevatorAndWrist(ElevatorConstants.Level.Down));
+		
+		//right side buttons
+		secondaryController.button(11).onTrue(algaeWristCommands.set(AlgaeWristState.EXTENDED));
+		secondaryController.button(12).onTrue(algaeWristCommands.set(AlgaeWristState.STOWED));
+		secondaryController.button(13).onTrue(climbCommands.goTo(ClimbConstants.Targets.MAX));
+		secondaryController.button(14).onTrue(climbCommands.goTo(ClimbConstants.Targets.TARGET));
 	}
 
 	/**
-	 * Use this to pass the autonomous command to the main {@link Robot} class.
-	 *
 	 * @return the command to run in autonomous
 	 */
 	public Command getAutonomousCommand() {
