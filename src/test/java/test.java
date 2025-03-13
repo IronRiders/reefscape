b5522b55f0762049626c6eb08686d79635dd4f50
saturncorgi <tylerclayton183@gmail.com>
2025-03-13 16:11:34 -0700

    example test

---
+++ src/test/java/test.java
@@ -0,0 +1,19 @@
+import java.beans.Transient;
+
+import org.ironriders.drive.DriveSubsystem;
+import org.ironriders.elevator.ElevatorCommands;
+import org.ironriders.elevator.ElevatorSubsystem;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class test {
+    DriveSubsystem driveSubsystem=new DriveSubsystem();
+    ElevatorSubsystem elevatorSubsystem=new ElevatorSubsystem();
+    ElevatorCommands elevatorCommands =elevatorSubsystem.getCommands();
+    @Test
+    void test(){
+        
+        assert(1==1);
+    }
+}
