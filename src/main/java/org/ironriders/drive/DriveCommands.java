03b250108869903e0f1565efb228bf951efe831a
isabelle <lshields822@gmail.com>
2025-03-14 09:45:04 -0700

    add better pathfind command using starting state

--- src/main/java/org/ironriders/drive/DriveCommands.java
+++ src/main/java/org/ironriders/drive/DriveCommands.java
@@ -1,5 +1,6 @@
 package org.ironriders.drive;
 
+import java.util.List;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
@@ -7,13 +8,22 @@ import java.util.function.Supplier;
 import org.ironriders.lib.GameState;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.IdealStartingState;
 import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static org.ironriders.elevator.ElevatorConstants.T;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -22,10 +32,29 @@ import edu.wpi.first.wpilibj2.command.Commands;
 public class DriveCommands {
 	private final DriveSubsystem driveSubsystem;
 
+	private final PathConstraints pathConstraints = new PathConstraints(
+		DriveConstants.SWERVE_MAXIMUM_SPEED_AUTO,
+		DriveConstants.SWERVE_MAXIMUM_ACCELERATION_AUTO,
+		DriveConstants.SWERVE_MAXIMUM_ANGULAR_VELOCITY_AUTO,
+		DriveConstants.SWERVE_MAXIMUM_ANGULAR_ACCELERATION_AUTO);
+	
+
 	public DriveCommands(DriveSubsystem driveSubsystem) {
 		this.driveSubsystem = driveSubsystem;
 
 		this.driveSubsystem.publish("Drive to Target", pathfindToTarget());
+	}
+
+	private LinearVelocity getVelocityMagnitude(ChassisSpeeds cs){
+        return MetersPerSecond.of(new Translation2d(cs.vxMetersPerSecond, cs.vyMetersPerSecond).getNorm());
+    }
+
+	private Rotation2d getPathVelocityHeading(ChassisSpeeds cs, Pose2d target){
+		if (getVelocityMagnitude(cs).in(MetersPerSecond) < 0.25) {
+			var diff = target.minus(driveSubsystem.getPose()).getTranslation();
+			return (diff.getNorm() < 0.01) ? target.getRotation() : diff.getAngle();//.rotateBy(Rotation2d.k180deg);
+		}
+		return new Rotation2d(cs.vxMetersPerSecond, cs.vyMetersPerSecond);
 	}
 
 	public Command drive(Supplier<Translation2d> translation, DoubleSupplier rotation, BooleanSupplier fieldRelative) {
@@ -78,12 +107,25 @@ public class DriveCommands {
 
 	public Command pathfindToPose(Pose2d targetPose) {
 		return driveSubsystem.defer(() -> {
-			driveSubsystem.pathfindCommand = AutoBuilder.pathfindToPose(targetPose, new PathConstraints(
-					DriveConstants.SWERVE_MAXIMUM_SPEED_AUTO,
-					DriveConstants.SWERVE_MAXIMUM_ACCELERATION_AUTO,
-					DriveConstants.SWERVE_MAXIMUM_ANGULAR_VELOCITY_AUTO,
-					DriveConstants.SWERVE_MAXIMUM_ANGULAR_ACCELERATION_AUTO));
-			return driveSubsystem.pathfindCommand;
+			List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
+            	new Pose2d(driveSubsystem.getPose().getTranslation(), getPathVelocityHeading(driveSubsystem.getSwerveDrive().getFieldVelocity(), targetPose))
+        	);
+
+			PathPlannerPath path = new PathPlannerPath(
+            	waypoints, 
+            	pathConstraints,
+            	new IdealStartingState(getVelocityMagnitude(driveSubsystem.getSwerveDrive().getFieldVelocity()), driveSubsystem.getSwerveDrive().getPose().getRotation()), 
+            	new GoalEndState(0.0, targetPose.getRotation())
+        	);
+
+			return (AutoBuilder.followPath(path)); // TODO: implement pid align
+
+			//driveSubsystem.pathfindCommand = AutoBuilder.pathfindToPose(targetPose, new PathConstraints(
+			//		DriveConstants.SWERVE_MAXIMUM_SPEED_AUTO,
+			//		DriveConstants.SWERVE_MAXIMUM_ACCELERATION_AUTO,
+			//		DriveConstants.SWERVE_MAXIMUM_ANGULAR_VELOCITY_AUTO,
+			//		DriveConstants.SWERVE_MAXIMUM_ANGULAR_ACCELERATION_AUTO));
+			//return driveSubsystem.pathfindCommand;
 		});
 	}
 
