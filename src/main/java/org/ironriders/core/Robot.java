4c94378abd045472b3b749eedbff92ad4ea430bc
iprobablydontknowwhatimdoing <octact82@gmail.com>
2025-03-17 16:51:24 -0700

    Fixed Dashboard

--- src/main/java/org/ironriders/core/Robot.java
+++ src/main/java/org/ironriders/core/Robot.java
@@ -4,6 +4,8 @@
 
 package org.ironriders.core;
 
+import edu.wpi.first.net.WebServer;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -23,6 +25,7 @@ public class Robot extends TimedRobot {
 
 	public Robot() {
 		robotContainer = new RobotContainer();
+		WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
 	}
 
 	@Override
