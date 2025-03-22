31f5ce657dc6bdda788afaa11043b66771caf889
Ryan Elbert <ryan.elbert5125@gmail.com>
2025-03-22 09:29:55 -0700

    commit prac

--- src/main/java/org/ironriders/drive/DriveConstants.java
+++ src/main/java/org/ironriders/drive/DriveConstants.java
@@ -33,7 +33,7 @@ public class DriveConstants {
     public static final double ROTATION_CONTROL_EXPONENT = 3.0;
     public static final double ROTATION_CONTROL_DEADBAND = 0.8;
 
-    public static final double SWERVE_DRIVE_MAX_SPEED = 5.0; // m/s
+    public static final double SWERVE_DRIVE_MAX_SPEED = 1.0; // m/s
     public static final double SWERVE_MAXIMUM_ANGULAR_VELOCITY_TELEOP = Math.PI * 4; // rad/s
 
     public static final double SWERVE_MAXIMUM_SPEED_AUTO = 2.0; // m/s
@@ -42,5 +42,5 @@ public class DriveConstants {
     public static final double SWERVE_MAXIMUM_ANGULAR_ACCELERATION_AUTO = SWERVE_MAXIMUM_ANGULAR_VELOCITY_AUTO / 2; // rad/s^2
 
     public static final double JOG_DISTANCE_INCHES = 0.5;
-    public static final double JOG_SPEED = .5;
+    public static final double JOG_SPEED = .25;
 }
